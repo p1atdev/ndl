ad9918511d9789ab3b19c02304d6784637b7016a
Plat <github@p1at.dev>
2022-08-17 22:40:34 +0900

    chore: update deps

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 // Deno's standard modules are here: https://deno.land/std
 
 // test
-export * from "https://deno.land/std@0.142.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.152.0/testing/asserts.ts";
 
 // xml
 export * as xml from "https://deno.land/x/xml@2.0.4/mod.ts";
